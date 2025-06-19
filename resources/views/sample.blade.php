4c37a57d8abebdd62449a04ae7a88425066860d0
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-06-19 14:44:59 +0900

    2025-6-12の授業内容

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,7 @@
+<html>
+
+<body>
+    <h1>Sample Page</h1>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\SampleController;
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
+
+Route::get('/sample', [SampleController::class, 'index']);
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {

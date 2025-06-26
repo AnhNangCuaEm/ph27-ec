5dff6bda02ca6930b7ee6c4f3207a1fe69e61c91
AnhNangCuaEm <102863074+AnhNangCuaEm@users.noreply.github.com>
2025-06-26 17:29:06 +0900

    26-6

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -13,4 +13,15 @@
             </li>
         @endforeach
     </ul>
+    <h2>セール商品一覧</h2>
+    <ul>
+        @foreach ($saleProducts as $saleProduct)
+            <li>
+                <a href="{{ route('products.show', ['id' => $saleProduct->id]) }}">
+                    {{ $saleProduct->name }}
+                </a>
+                {{ $saleProduct->price }} 円
+            </li>
+        @endforeach
+    </ul>
 @endsection
